cbf1abd6eaf5ff1d2b1e23554713cae8829cf0e5
Njosephine <josephinenanyonga389@gmail.com>
2025-04-23 16:11:49 +0300

    changed to postgres

--- AutomatedSystem/settings.py
+++ AutomatedSystem/settings.py
@@ -81,10 +81,22 @@ ASGI_APPLICATION = 'AutomatedSystem.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'farm',
+        'USER': 'farm',
+        'PASSWORD': 'FARM',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 
 
 # Password validation
