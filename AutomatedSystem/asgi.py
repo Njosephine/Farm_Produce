9472fd4180da813f051c7eb98ef84432b24b2d73
Njosephine <josephinenanyonga389@gmail.com>
2025-04-24 15:54:20 +0300

    added Docker to the system

--- AutomatedSystem/asgi.py
+++ AutomatedSystem/asgi.py
@@ -1,17 +1,8 @@
 import os
-import dashboard.routing
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 
 # Set the correct settings module for the project
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AutomatedSystem.settings')
 
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            dashboard.routing.websocket_urlpatterns
-        )
-    )
-})
+# Set the application to be the default ASGI application for HTTP requests
+application = get_asgi_application()
