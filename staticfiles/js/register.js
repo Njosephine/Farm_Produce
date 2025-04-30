663e22f86f7ffb77b031d05ebc90e533393eeea2
Njosephine <josephinenanyonga389@gmail.com>
2025-04-30 11:13:49 +0300

    production

---
+++ staticfiles/js/register.js
@@ -0,0 +1,9 @@
+
+
+ // Import MDB components
+    
+  import { Input, Tab, Ripple, initMDB } from "https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/8.2.0/mdb.es.min.js";
+
+// Initialize the necessary MDB components
+initMDB({ Input, Tab, Ripple });
+
